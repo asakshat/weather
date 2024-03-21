bd9328ba2d41bef304b31d921464d79dfcb702e2
asakshat <asakshat453@gmail.com>
2024-03-21 10:28:58 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,93 @@
+const form = document.getElementById('weather-form');
+const celc = -273.15;
+const generateTemperatureHTML = (forecastData) => {
+	let html = '';
+	forecastData.forEach((day) => {
+		const date = new Date(day.date);
+		const formattedDate = date.toLocaleDateString('en-US', {
+			weekday: 'short',
+			month: 'long',
+			day: 'numeric',
+		});
+
+		const iconUrl = day.day.condition.icon;
+
+		html += `
+            <div class="forecast-day">
+                <h3>${formattedDate}</h3>
+                <img src="${iconUrl}" alt="Weather Icon"> <!-- Display weather icon -->
+                <p>Avg Temperature: ${day.day.avgtemp_c}°C</p>
+                <p>Max Temperature: ${day.day.maxtemp_c}°C</p>
+                <p>Min Temperature: ${day.day.mintemp_c}°C</p>
+            </div>
+        `;
+	});
+	return html;
+};
+
+form.addEventListener('submit', async (e) => {
+	e.preventDefault();
+	const forecastContainer = document.querySelector('.forecast-container');
+	forecastContainer.innerHTML = '';
+
+	const cityName = document.getElementById('city').value;
+	const h2 = document.createElement('h2');
+	h2.textContent = cityName;
+	forecastContainer.appendChild(h2);
+
+	const apiURL = `https://api.weatherapi.com/v1/forecast.json?key=4b149762fd9240b0897142727242003&q=${cityName}&days=5`;
+
+	const response = await fetch(apiURL);
+	const data = await response.json();
+	console.log(data);
+
+	const forecastData = data.forecast.forecastday;
+	const temperatureHTML = generateTemperatureHTML(forecastData);
+	forecastContainer.innerHTML = temperatureHTML;
+
+	const temperatureData = forecastData.map((day) => ({
+		date: new Date(day.date).toLocaleDateString('en-US', {
+			weekday: 'short',
+			month: 'long',
+			day: 'numeric',
+		}),
+		avgTemperature: day.day.avgtemp_c,
+	}));
+
+	new Chart(document.getElementById('chart'), {
+		type: 'line',
+		data: {
+			labels: temperatureData.map((day) => day.date),
+			datasets: [
+				{
+					label: 'Average Temperature (°C)',
+					data: temperatureData.map((day) => day.avgTemperature),
+					borderColor: 'rgba(255,99,132,1)',
+					borderWidth: 1,
+					backgroundColor: 'rgba(255, 99, 132, 0.2)',
+				},
+			],
+		},
+		options: {
+			scales: {
+				y: {
+					beginAtZero: false,
+					grid: {
+						color: 'rgba(255,255,255,0.2)',
+					},
+					ticks: {
+						color: 'rgba(255,255,255,0.7)',
+					},
+				},
+				x: {
+					grid: {
+						color: 'rgba(255,255,255,0.2)',
+					},
+					ticks: {
+						color: 'rgba(255,255,255,0.7)',
+					},
+				},
+			},
+		},
+	});
+});
